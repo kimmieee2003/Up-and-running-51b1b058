086d4ad837100b37c835b953308f4996125e88d2
Kimberly Hogeveen <2103405@talnet.nl>
2020-04-22 10:36:13 +0200

    verbeter

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 $dbh = new PDO(
-    'mysql:host=127.0.0.1;port:3306;dbname=netland', 'root', ''
+    'mysql:host=127.0.0.1;port:3306;dbname=netland', 
+    'root', 
+    ''
 );
 echo 'connected to Netland';
 echo $dbh->query('select version()')->fetchColumn();
